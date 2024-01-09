5788a8a9db84379e713db8e5865c533cb1f67f6e
omer geva <118137246+Omeva90@users.noreply.github.com>
2024-01-09 12:33:55 +0200

    Add files via upload

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [["./src/**/*.{html,js}  ./node_modules/flowbite/**/*.js  'node_modules/preline/dist/*.js' "]],
+  theme: {
+    extend: {},
+  },
+  plugins: [ 
+    require('preline/plugin'),
+    require('flowbite/plugin ')
+],
+}
+
